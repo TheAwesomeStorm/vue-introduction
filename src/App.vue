4f037f41a129cfae16ef659f178ede40db398415
Daniel Mendes Sechinske <mendes.sechinske@gmail.com>
2022-07-25 18:50:26 -0300

    Removed HelloWorld component, added components LateralMenu and TaskForms

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <LateralMenu />
+    </div>
+    <div class="column is-three-quarters">
+      <TaskForms />
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import LateralMenu from '@/components/LateralMenu.vue';
+import TaskForms from '@/components/TaskForms.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    LateralMenu,
+    TaskForms
   }
 });
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    margin: 0;
+  }
 </style>
---
+++ src/components/TaskForms.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div class="column is-8" role="form" aria-label="A form to create new tasks">
+        <input type="text" class="input" placeholder="Write a task to start">
+      </div>
+      <div class="column">
+        <div class="is-flex is-align-items-center is-justify-content-space-between">
+          <section>
+            <strong>{{ timeElapsed }}</strong>
+          </section>
+          <button class="button" @click="startCountdown">
+            <span class="icon">
+              <i class="fas fa-play" />
+            </span>
+            <span>Play</span>
+          </button>
+          <button class="button" @click="stopCountdown">
+            <span class="icon">
+              <i class="fas fa-stop" />
+            </span>
+              <span>Stop</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TaskForms',
+  computed: {
+    timeElapsed (): string {
+      return new Date(1000 * this.timeInSeconds).toISOString().substring(19, 11);
+    }
+  },
+  data () {
+    return {
+      timeInSeconds: 0,
+      stopwatch: 0
+    }
+  },
+  methods: {
+    startCountdown () {
+      this.stopwatch = setInterval(() => {
+        this.timeInSeconds += 1;
+      }, 1000)
+    },
+    stopCountdown () {
+      clearInterval(this.stopwatch);
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
