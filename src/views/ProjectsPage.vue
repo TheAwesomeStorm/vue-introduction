d58bb6cbd17e72cc66a74a20186bf499dabc96cf
Daniel Mendes Sechinske <mendes.sechinske@gmail.com>
2022-07-26 10:43:52 -0300

    Added interface IProject to type object created at ProjectsPage

--- src/views/ProjectsPage.vue
+++ src/views/ProjectsPage.vue
@@ -1,15 +1,70 @@
 <template>
-  <h1>Projects</h1>
+  <section class='projects'>
+    <h1 class='title'>
+      Projects
+    </h1>
+    <form @submit.prevent='save'>
+      <div class='field'>
+        <label for='projectName' class='label'>
+          Project Name
+        </label>
+        <input type='text' class='input' v-model='projectName' id='projectName'>
+      </div>
+      <div class='field'>
+        <button class='button' type='submit' :disabled='isProjectNameEmpty'>
+          Save
+        </button>
+      </div>
+    </form>
+    <table class='table is-fullwidth'>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for='project in projects' :key='project.id'>
+          <td>{{ project.id }}</td>
+          <td>{{ project.name }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
 </template>
 
-<script>
+<script lang='ts'>
+import { IProject } from '@/interfaces/IProject';
 import {defineComponent} from "vue";
 
 export default defineComponent ({
+  computed: {
+    isProjectNameEmpty (): boolean {
+      return this.projectName.length === 0;
+    }
+  },
+  data () {
+    return {
+      projectName: '',
+      projects: [] as IProject[]
+    };
+  },
+  methods: {
+    save () {
+      const project: IProject = {
+        id: new Date().toISOString(),
+        name: this.projectName
+      };
+      this.projects.push(project);
+      this.projectName = '';
+    }
+  },
   name: "ProjectsPage"
 });
 </script>
 
 <style scoped>
-
+  .projects {
+    padding: 1.25rem;
+  }
 </style>
